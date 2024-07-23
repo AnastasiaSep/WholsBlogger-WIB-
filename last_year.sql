b33c8db6a891e8d92f2670dfc8955fa7c1e70b62
Anastasia <nastyadelonge_1@mail.ru>
2024-07-23 18:40:26 +0300

    Update last_year.sql

--- last_year.sql
+++ last_year.sql
@@ -9,18 +9,3 @@ WHERE EXTRACT(year FROM p.date) = 2024
 GROUP BY i.itemId
 ORDER BY item_income DESC
 LIMIT 1;
-
---
---WITH LastYear AS (
---  SELECT MAX(EXTRACT(YEAR FROM p.date)) AS last_year
---  FROM Purchases p
---)
---SELECT
---  i.itemId,
---  SUM(i.price) AS item_income
---FROM Purchases p
---JOIN Items i ON p.itemId = i.itemId
---JOIN LastYear ly ON EXTRACT(YEAR FROM p.date) = ly.last_year
---GROUP BY i.itemId
---ORDER BY item_income DESC
---LIMIT 1;
